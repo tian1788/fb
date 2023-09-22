33c488ecd8ed5636a60e60f817ba376d79c6ceb7
tian1788 <christianperlas28@gmail.com>
2023-09-22 10:06:43 +0000

    try

---
+++ main.js
@@ -0,0 +1,8 @@
+let name = prompt("ano name mo?");
+alert("namo ka " + name);
+alert("namo ka talaga " + name);
+alert("charot lang");
+alert("bat ang tamad mo ngayun? " + name);
+alert("r u ok?");
+alert("kung hindi kawawi ka naman HAHAHAHA");
+alert("namo ka promise " + name);
