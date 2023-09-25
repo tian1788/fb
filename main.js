da2d6301beab31bc25b1ff2eed210569e57fb4d5
tian1788 <christianperlas28@gmail.com>
2023-09-25 16:04:36 +0000

    eto

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let name = prompt("ano name mo?");
-alert("namo ka " + name);
-alert("namo ka talaga " + name);
-alert("charot lang");
-alert("r u ok?");
-alert("bat ang tamad mo ngayun? " + name);
-alert("kung hindi kawawi ka naman HAHAHAHA");
-alert("de jwk lang");
-alert("Eto konting kacornyhan ko HAAHAHAHA");
+// let name = prompt("ano name mo?");
+// alert("namo ka " + name);
+// alert("namo ka talaga " + name);
+// alert("charot lang");
+// alert("r u ok?");
+// alert("bat ang tamad mo ngayun? " + name);
+// alert("kung hindi kawawi ka naman HAHAHAHA");
+// alert("de jwk lang");
+// alert("Eto konting kacornyhan ko HAAHAHAHA");
+let toggle = document.getElementById('toggle');
+let box = document.querySelector(".box");
+let isWide = false;
+
+toggle.addEventListener("click", ()=> {
+    if(isWide == false) {
+        box.style.left = "0";
+        isWide = true;
+    } else if(isWide == true){
+        box.style.left = "-30%";
+        isWide = false;
+    }
+});
